0c94c5d9212d019a0b3d8da30106ebe573961095
Sahin Reja <sahin.reja@indusnet.co.in>
2023-12-28 18:09:23 +0530

    First Commit

---
+++ src/main/java/com/ecommerce/springecomrest/SpringEcomRestApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerce.springecomrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringEcomRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringEcomRestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ecommerce/springecomrest/SpringEcomRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommerce.springecomrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringEcomRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
